0b0765304382c5372eae79820f6b703c956c041e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-13 11:39:04 +0100

    Add the dependency of es6-promise and implement get

--- straw-ios-service-http.js
+++ straw-ios-service-http.js
@@ -1,13 +1,19 @@
 // straw-ios-service-http.js
 
+// This library depends on es6 Promise.
+
 /**
  * @class
  * @singleton
  */
-straw.service.http = (function () {
+straw.service.http = (function (straw) {
     'use strict';
 
     var exports = {};
+
+    var core = straw.core;
+
+    var Promise = window.Promise;
 
     /**
      * @method
@@ -15,8 +21,17 @@ straw.service.http = (function () {
      *
      * @param {String} url The url to get.
      * @param {Object} opts The options. "timeout": timeout period in milliseconds, "charset": character sets, for example "utf8", "shift_jis" or "euc-jp".
+     * @return {Promise} The promise object.
      */
     exports.get = function (url, opts) {
+
+        var promise = new Promise(function (resolve, reject) {
+
+            core.serviceCall('http', 'get', {url: url, timeout: opts.timeout, charset: opts.charset}, resolve, reject);
+
+        });
+
+        return promise;
     };
 
     /**
@@ -30,4 +45,4 @@ straw.service.http = (function () {
 
     return exports;
 
-}());
+}(window.straw));
