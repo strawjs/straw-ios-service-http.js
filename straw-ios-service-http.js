f3029b4ac38de7ba7ce8644ac4634b45192afaca
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-14 15:18:03 +0100

    Add docs

--- straw-ios-service-http.js
+++ straw-ios-service-http.js
@@ -19,6 +19,16 @@ straw.service.http = (function (straw) {
      * @method
      * Perform `GET` method.
      *
+     * The promise object resolves when you get the HTTP response and otherwise be rejected.
+     *
+     * ### When resolved, it has:
+     * - {String} `responseText` The response text.
+     * - {Number} `statusCode` The status code (ex. 200, 404, 500, 503)
+     *
+     * ### When rejected, it has:
+     * - {Number} `code` The error code.
+     * - {String} `message` The error message
+     *
      * @param {String} url The url to get.
      * @param {Object} opts The options. "timeout": timeout period in milliseconds, "charset": character sets, for example "utf8", "shift_jis" or "euc-jp".
      * @return {Promise} The promise object.
@@ -36,7 +46,7 @@ straw.service.http = (function (straw) {
 
     /**
      * @method
-     * Perform `GET` method.
+     * Perform `POST` method.
      *
      * @param {String} url
      */
