78d4dd813a9fc81747292e2a9ed96f653550a2bc
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-16 16:59:16 +0100

    Add necessary components for the test

--- straw-ios-service-http-spec.js
+++ straw-ios-service-http-spec.js
@@ -1,11 +1,19 @@
-/* global describe, it */
+/* global straw, describe, it, chai */
+
+var expect = chai.expect;
 
 describe('straw.service.http', function () {
     'use strict';
 
+    it('is a object', function () {
+        expect(straw.service.http).to.be.a('object');
+    });
+
+
     describe('get', function () {
 
-        it('is true', function () {
+        it('is a function', function () {
+            expect(straw.service.http.get).to.be.a('function');
         });
 
     });
