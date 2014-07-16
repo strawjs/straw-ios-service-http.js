3196853a02a0eacc5b5f452fdd18d926b928559e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-16 23:28:55 +0100

    Add the test of get

--- straw-ios-service-http-spec.js
+++ straw-ios-service-http-spec.js
@@ -1,4 +1,4 @@
-/* global straw, describe, it, chai */
+/* global straw, describe, it, chai, sinon */
 
 var expect = chai.expect;
 
@@ -10,10 +10,22 @@ describe('straw.service.http', function () {
     });
 
 
-    describe('get', function () {
+    describe('.get', function () {
 
         it('is a function', function () {
             expect(straw.service.http.get).to.be.a('function');
+        });
+
+        it('calls straw.core.serviceCall', function () {
+
+            var mock = sinon.mock(straw.core);
+            mock.expects('serviceCall').once();
+
+            straw.service.http.get('http://www.example.com/', {charset: 'utf8', timeout: 3000});
+
+            mock.verify();
+            mock.restore();
+
         });
 
     });
